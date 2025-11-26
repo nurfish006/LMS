66ff3cbe9ce8897bae98b8bc1f6bfd09201d7bf8
nurfish006 <nurfish06@gmail.com>
2025-11-26 08:31:03 +0300

    Fix Invalid namespace

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -43,7 +43,7 @@ function getClientPromise(): Promise<MongoClient> {
 
 export async function getDatabase(): Promise<Db> {
   const client = await getClientPromise()
-  return client.db("Bahir Dar_els")
+  return client.db("Bahir_Dar_els")
 }
 
 export const getDb = getDatabase
